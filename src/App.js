63742d3923c22337ae9f1aeb54d6e3e54a7c80d1
Lenny Thach <lennythach1@gmail.com>
2022-06-29 15:15:33 -0700

    adding css and audio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import SingleCard from './components/SingleCard'
+import { Howl } from 'howler'
 
+//---------------Source-------------------//
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/BlueEyesWhiteDragon.png", matched: false },
+  { "src": "/img/DarkMagician.png", matched: false },
+  { "src": "/img/DarkMagicianGirl.png", matched: false },
+  { "src": "/img/ObeliskTheTormentor.png", matched: false },
+  { "src": "/img/SliferTheSkyDragon.png", matched: false },
+  { "src": "/img/TheWingedDragonOfRa.png", matched: false },
 ]
 
+const yugiohSounds = {
+  newGame:'/audio/its-time-to-dd-d-duel-yugioh.mp3',
+  matched:'/audio/duel-disk-yugioh-begin-activate-sound.mp3'
+}
+
 function App() {
+  //-----------------States----------------//
   const [cards,setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choice1, setChoice1] = useState(null);
   const [choice2, setChoice2] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
-
-
+  //----------------Functions---------------//
   const handleChoice = (card) => {
     choice1 ? setChoice2(card) : setChoice1(card)
   }
@@ -27,22 +35,14 @@ function App() {
     setChoice1(null)
     setChoice2(null)
     setTurns(prevTurn => prevTurn + 1)
+    setDisabled(false)
+    setChoice1(null)
+    setChoice2(null)
   }
-
-  useEffect(() => {
-    if (choice1 && choice2) {
-      if (choice1.src === choice2.src) {
-        console.log("They Both match!")
-        resetChoice()
-      } else {
-        console.log('They Do not match!')
-        resetChoice()
-      }
-    }
-  },[choice1, choice2]) 
 
   const shuffleCards = () => {
     resetChoice()
+    callSound(yugiohSounds.newGame)
     const shuffleCards = [...cardImages, ...cardImages]
       .sort(()=>Math.random() - .5)
       .map((card)=>({...card, id:Math.random()}))
@@ -51,15 +51,56 @@ function App() {
     setTurns(0)
   }
 
+  const callSound = (src) => {
+    const sound = new Howl({
+      src,
+      html5:true
+    })
+    sound.play()
+  }
+  //----------------------useEffect----------------------//
+  useEffect(() => {
+    if (choice1 && choice2) {
+      setDisabled(true)
+      if (choice1.src === choice2.src) {
+        setCards(prevCard => {
+          return prevCard.map(card => {
+            if (card.src === choice1.src) {
+              callSound(yugiohSounds.matched)
+              return {...card, matched:true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetChoice()
+      } else {
+        console.log('They Do not match!')
+        setTimeout(()=>resetChoice(),1000)
+      }
+    }
+  },[choice1, choice2]) 
+
+  useEffect(()=>{
+    return shuffleCards()
+  },[])
+
+
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Yu Gi Oh Matching Game!</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
           {cards.map((card)=>(
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard key={card.id} 
+          card={card} 
+          flipped={card === choice1 || card === choice2 || card.matched} 
+          handleChoice={handleChoice} 
+          disabled={disabled} 
+          />
           ))}
       </div>
+      <p>Turns:{turns}</p>
     </div>
   );
 }
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,18 @@
 import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+
+export default function SingleCard({card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+  }
   return (
     <div className='card'>
-        <div >
+        <div className={flipped?"flipped":""}>
             <img className='front' src={card.src} alt="card front" />
             <img className='back' 
-            onClick={()=>{handleChoice(card)}} 
+            onClick={handleClick} 
             src={'/img/cover.png'} 
             alt="cover" />
         </div>
